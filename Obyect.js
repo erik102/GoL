3c2b329ba19d706c1a00e613e45593f879bfbfb9
erik102 <erik.tovmasyan.s@tumo.org>
2021-07-02 17:49:12 +0400

    avelacrac failer

---
+++ Obyect.js
@@ -0,0 +1,128 @@
+class Obyect {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 8;
+        this.multiply = 0
+        this.directions = [];
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    
+
+    move() {
+        this.energy--
+        var emptyCells = this.chooseCell(0)
+        var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+
+        if (newCell && this.energy >= 0) {
+            console.log(newCell)
+            var newX = newCell[0]
+            var newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        }
+       
+        }
+
+        eat() {
+            var emptyCells = this.chooseCell(2)
+            var newCell= emptyCells[Math.floor(Math.random() * emptyCells.length)]
+    
+            var emptyCells1 = this.chooseCell(3)
+            var newCell1 = emptyCells1[Math.floor(Math.random() * emptyCells1.length)]
+    
+            var emptyCells2 = this.chooseCell(1)
+            var newCell2= emptyCells2[Math.floor(Math.random() * emptyCells2.length)]
+    
+            if (newCell) {
+                this.energy++
+                var newX = newCell[0]
+                var newY = newCell[1]
+    
+                matrix[newY][newX] = matrix[this.y][this.x]
+                matrix[this.y][this.x] = 0
+                this.x = newX
+                this.y = newY
+                for (var i in grassEaterArr) {
+                    if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+                        grassEaterArr.splice(i, 1)
+                        break
+                    }
+                }
+                
+                
+            }
+           else if (newCell1) {
+                this.energy++
+                var newX = newCell1[0]
+                var newY = newCell1[1]
+    
+                matrix[newY][newX] = matrix[this.y][this.x]
+                matrix[this.y][this.x] = 0
+                this.x = newX
+                this.y = newY
+                for (var i in predatorArr) {
+                    if (newX == predatorArr[i].x && newY == predatorArr[i].y) {
+                        predatorArr.splice(i, 1)
+                        break
+                    }
+                }
+                
+                
+            }
+            else if (newCell2) {
+                this.energy++
+                var newX = newCell2[0]
+                var newY = newCell2[1]
+    
+                matrix[newY][newX] = matrix[this.y][this.x]
+                matrix[this.y][this.x] = 0
+                this.x = newX
+                this.y = newY
+                for (var i in grassArr) {
+                    if (newX == grassArr[i].x && newY == grassArr[i].y) {
+                        grassArr.splice(i, 1)
+                        break
+                    }
+                }
+                
+                
+            }
+            
+            else {
+                this.move()
+            }
+        }
+
+    }
